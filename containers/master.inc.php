9b613a63209227cd7f6fbecc3f0534d01db11339
mahmouddaoud972 <mahmoud.daoud@esprit.tn>
2024-10-20 19:50:45 +0100

    b2b fastmag

---
+++ containers/master.inc.php
@@ -0,0 +1,7 @@
+<?php
+// File generated to link to the master file - DO NOT MODIFY - It is just an include
+if (! defined('USEDOLIBARRSERVER') && ! defined('USEDOLIBARREDITOR')) {
+    if (! defined('USEEXTERNALSERVER')) define('USEEXTERNALSERVER', 1);
+    require_once '/home/clients/936acf88b8cf769adc7ea01a04725743/sites/shop.cieloo.io/master.inc.php';
+}
+?>
